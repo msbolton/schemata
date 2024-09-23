1626d4b54aff50f59452848b3ad7dbb48add52cc
Michael Bolton <msbolton@protonmail.com>
2024-09-23 14:38:23 -0400

    feat(idl): created initial schemata grammar
    closes #1

---
+++ src/main/kotlin/com/github/msbolton/schemata/SchemataCommand.kt
@@ -0,0 +1,26 @@
+package com.github.msbolton.schemata
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+
+@Command(
+    name = "schemata",
+    description = ["Schemata CLI application"],
+    mixinStandardHelpOptions = true,
+    subcommands = [GenerateCommand::class, ValidateCommand::class]
+)
+class SchemataCommand : Runnable {
+
+    override fun run() {
+        // Default action if no subcommand is provided
+        println("Schemata CLI - Use --help for more information.")
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            PicocliRunner.run(SchemataCommand::class.java, *args)
+        }
+    }
+}
